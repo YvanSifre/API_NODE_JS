6a4d2ba292b994780ef13eb7dd1addc5c5e05ef5
Yvan Sifre <YvanSifre1@MacBook-Pro-de-Yvan.local>
2022-04-01 10:53:48 +0200

    ajout des tests

---
+++ cours_node_Yvan/tests/calc.mjs
@@ -0,0 +1,13 @@
+const Add = (a, b) => {
+    return a + b
+}
+
+const Substract = (a, b) => {
+    return a - b
+}
+
+const Multiply = (a, b) => {
+    return a * b
+}
+
+export { Add, Substract, Multiply }
---
+++ cours_node_Yvan/tests/calc.test.js
@@ -0,0 +1,15 @@
+import { Add, Substract, Multiply } from "./calc.mjs"
+
+//Test de la fonction Add()
+test('Addition : 5+5', () => {
+    expect(Add(5, 5)).toBe(10);
+});
+
+//Test de la fonction Substract()
+test('Soustraction : 7-4', () => {
+    expect(Substract(7, 4)).toBe(3);
+});
+
+test('Multiplication : 3*4', () => {
+    expect(Multiply(3, 4)).toBe(12);
+}); 
